21263475aabd4201399fd3db2fabb1cc319e7346
syan212 <syan235711@gmail.com>
2025-05-22 18:01:05 +0100

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,10 +1,25 @@
 import random
-import sympy
 import math
+def isPrime(n, k=5): # miller-rabin
+    if n < 2: return False
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: return n == p
+    s, d = 0, n-1
+    while d % 2 == 0:
+        s, d = s+1, d/2
+    for i in range(k):
+        x = pow(random.randint(2, n-1), d, n)
+        if x == 1 or x == n-1: continue
+        for r in range(1, s):
+            x = (x * x) % n
+            if x == 1: return False
+            if x == n-1: break
+        else: return False
+    return True
 def generate_prime(bits):
     while True:
         prime_candidate = random.getrandbits(bits)
-        if sympy.isprime(prime_candidate):
+        if isPrime(prime_candidate):
             return prime_candidate
 def generate_keys():
     p = generate_prime(1024)
